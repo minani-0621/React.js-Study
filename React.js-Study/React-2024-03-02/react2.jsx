f687389581df0311d1c22e955c990ac075517f27
cm2hcm2h <cm2hcm2h@naver.com>
2024-03-03 00:10:46 +0900

    2024-03-02 State.v1

---
+++ React.js-Study/React-2024-03-02/react2.jsx
@@ -0,0 +1,21 @@
+const root = document.getElementById("root");
+
+let counter = 0;
+
+function countIp(){
+    counter = counter + 1;
+    render();
+}
+
+function render(){
+    ReactDOM.render(<Container />, root);
+}
+
+const Container = () => (
+    <div>
+        <h3>Total clicks: {counter}</h3>
+        <button onClick={countIp}>Click me</button>
+    </div>
+)
+/* 바닐라js에서는 이벤트 리스너를 통해서 갱신된 데이터를 다시 렌더링 할 때, 데이터가 담겨있는 태그요소 전체를 
+리렌더링 하는데, React.js에서는 html요소가 아니라 오로지 갱신된 데이터 그 자체만을 리렌더링하는 장점이 있다. */
