0fd298fcf4e7dfb8c84104fbdfedae178d22bf1f
cm2hcm2h <cm2hcm2h@naver.com>
2024-03-03 17:49:01 +0900

    오타 수정

--- React.js-Study/React-2024-03-02/react2.jsx
+++ React.js-Study/React-2024-03-02/react2.jsx
@@ -2,7 +2,7 @@ const root = document.getElementById("root");
 
 let counter = 0;
 
-function countIp(){
+function countUp(){
     counter = counter + 1;
     render();
 }
@@ -14,7 +14,7 @@ function render(){
 const Container = () => (
     <div>
         <h3>Total clicks: {counter}</h3>
-        <button onClick={countIp}>Click me</button>
+        <button onClick={countUp}>Click me</button>
     </div>
 )
 /* 바닐라js에서는 이벤트 리스너를 통해서 갱신된 데이터를 다시 렌더링 할 때, 데이터가 담겨있는 태그요소 전체를 
