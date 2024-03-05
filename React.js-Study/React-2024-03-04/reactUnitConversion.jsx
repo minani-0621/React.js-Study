081d257ebb746f1799a74fbd9f72bcf35148a0c4
cm2hcm2h <cm2hcm2h@naver.com>
2024-03-05 23:22:30 +0900

    2024-03-05 단위 변환 프로그램: Flip(모드변경)기능 추가

--- React.js-Study/React-2024-03-04/reactUnitConversion.jsx
+++ React.js-Study/React-2024-03-04/reactUnitConversion.jsx
@@ -1,35 +1,45 @@
 const root = document.getElementById("root");
 
 function App() {
-    const [minutes, setMinutes] = React.useState(0);
+    const [amount, setAmount] = React.useState(0);
+    const [flipped, setFlipped] = React.useState(false); // 분->시, 시->분 모드를 구별하기 위한 flipped State 추가 생성
     const onChange = (event) => {
-        setMinutes(event.target.value);
+        setAmount(event.target.value);
     };
-    const reset = () => {setMinutes(0)};
+    const reset = () => {
+        setAmount(0);
+    };
+    const onFlip = () => {
+        reset();
+        setFlipped(!flipped);
+    };
     return (
         <div>
             <h1>Super Converter</h1>
             <div>
                 <label htmlFor="minutes">Minutes </label>
                 <input 
-                    value={minutes} 
+                    value={flipped ? amount * 60 : amount} 
                     id="minutes" 
                     placeholder="Minutes" 
                     type="number" 
                     onChange={onChange}
+                    disabled={flipped}
                 />
             </div>
             <div>
                 <label htmlFor="hours">Hours </label>
                 <input 
-                    value={minutes / 60} 
+                    value={flipped ? amount : amount / 60} 
                     id="hours" 
                     placeholder="Hours" 
                     type="number" 
-                    disabled
+                    onChange={onChange} 
+                    disabled={!flipped}
                 />
             </div>
             <button onClick={reset}>Reset</button>
+            <button onClick={onFlip}>Flip</button>
         </div>
     );
 }
