065507d140156c270fbb93b894350511e17323a4
cm2hcm2h <cm2hcm2h@naver.com>
2024-03-07 23:33:56 +0900

    2024-03-07 단위 변환 프로그램: KmToMile 컴포넌트 일부 생성

--- React.js-Study/React-2024-03-04/reactUnitConversion.jsx
+++ React.js-Study/React-2024-03-04/reactUnitConversion.jsx
@@ -2,7 +2,7 @@ const root = document.getElementById("root");
 
 function MinutesToHours() {
     const [amount, setAmount] = React.useState(0);
-    const [inverted, setInverted] = React.useState(false); // 분->시, 시->분 모드를 구별하기 위한 inverted State 추가 생성
+    const [inverted, setInverted] = React.useState(false);
     const onChange = (event) => {
         setAmount(event.target.value);
     };
@@ -43,8 +43,46 @@ function MinutesToHours() {
     );
 }
 
-function KmToMiles() {
-    return <h3>Km to M</h3>;
+function KmToMile() {
+    const [distance, setDistance] = React.useState(0);
+    const [inverted, setInverted] = React.useState(false);
+    const onChange = (event) => {
+        setDistance(event.target.value);
+    }
+    const reset = () => {
+        setDistance(0);
+    }
+    const onInvert = () => {
+        setInverted(!inverted);
+    }
+    return (
+        <div>
+            <div>
+                <label htmlFor="km">Km </label>
+                <input 
+                    value={!inverted ? distance : distance * 1.609344}
+                    id="km"
+                    type="number" 
+                    placeholder="Km"
+                    onChange={onChange}
+                    disabled={inverted}
+                />
+            </div>
+            <div>
+                <label htmlFor="mile">Mile </label>
+                <input 
+                    value={inverted ? distance : distance / 1.609344}
+                    id="mile"
+                    type="number" 
+                    placeholder="Mile"
+                    onChange={onChange}
+                    disabled={!inverted}
+                />
+            </div>
+            <button onClick={reset}>Reset</button>
+            <button onClick={onInvert}>Invert</button>
+        </div>
+    );
 }
 
 function App() {
@@ -55,7 +93,8 @@ function App() {
     return (
         <div>
             <h1>Super Converter</h1>
-            <select onChange={onSelect}>
+            <label htmlFor="selectMode">Mode </label>
+            <select id="selectMode" onChange={onSelect}>
                 <option value="-1">Select your units</option>
                 <option value="0">Minutes & Hours</option>
                 <option value="1">Km & Miles</option>
@@ -63,7 +102,7 @@ function App() {
             <hr/>
             {index === "-1" ? <h3>Please select your units</h3> : null}
             {index === "0" ? <MinutesToHours /> : null}
-            {index === "1" ? <KmToMiles /> : null}
+            {index === "1" ? <KmToMile /> : null}
         </div>
     );
 }
