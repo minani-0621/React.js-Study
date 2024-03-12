54fbd57d03af6b1f329bfdb2e5184fdc14f2c4fc
cm2hcm2h <cm2hcm2h@naver.com>
2024-03-12 22:11:27 +0900

    2024-03-12 Props.v2-React.memo() 설명

--- React.js-Study/React-Props/react-props.jsx
+++ React.js-Study/React-Props/react-props.jsx
@@ -17,13 +17,18 @@ function Btn({text, onClick}) { {/* 첫번째 Btn 컴포넌트에 인자로 onCl
     )
 }
 
+{/* 부모 컴포넌트의 state를 변경하면 당연히 자식 컴포넌트들도 리렌더링이 일어나는데, 이 때 React.memo()로 prop의 변경이 일어난 부분만 리렌더링 시킬 수 있다.
+어떤 부모 컴포넌트가 수 많은 자식 컴포넌트를 가지고 있는 경우 React.memo()를 통해 불필요한 리렌더링을 줄일 수 있는 것이다. 컴포넌트가 React.memo()로 wrapping 될 때, 
+React는 컴포넌트를 렌더링하고 결과를 메모이징한다. 그리고 다음 렌더링이 일어날 때 props가 같다면, React는 메모이징된 내용을 재사용한다. */}
+const MemorizedBtn = React.memo(Btn); 
+
 function App() {
     const [value, setValue] = React.useState("Save Changes");
     const changeValue = () => setValue("Revert Changes");
     return (
         <div>
-            <Btn text={value} onClick={changeValue} /> {/* App 컴포넌트에서 첫번째 Btn에 onClick은 onClick 리스너가 아니라 그저 prop의 이름일 뿐이다. onClick prop은 changesValue 함수를 갖는데, 이 함수는 App 컴포넌트의 state인 value의 값을 "Revert Changes"로 바꿔준다. */}
-            <Btn text="Continue" />
+            <MemorizedBtn text={value} onClick={changeValue} /> {/* App 컴포넌트에서 첫번째 Btn에 onClick은 onClick 리스너가 아니라 그저 prop의 이름일 뿐이다. onClick prop은 changesValue 함수를 갖는데, 이 함수는 App 컴포넌트의 state인 value의 값을 "Revert Changes"로 바꿔준다. */}
+            <MemorizedBtn text="Continue" />
         </div>
     )
 }
