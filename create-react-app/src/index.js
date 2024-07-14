b204db97658e3ab5c9c2904f3a6d59db676381ed
minani-0621 <cm2hcm2h@naver.com>
2024-07-14 18:08:11 +0900

    2024-07-14 To Do List 만들기

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ToDoApp from './ToDoApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <ToDoApp />
 );
